f9476ead4c13456b918df225f5b552b530b1892f
yanis <yann.amrouche@gmail.com>
2021-10-01 18:58:39 +0200

    IHM

---
+++ src/FloorModel.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class FloorModel extends DefaultComboBoxModel {
+    ArrayList<String> floors;
+    public FloorModel(){
+        super();
+        floors = new ArrayList<>();
+        floors.add("Etage : 0");
+        floors.add("Etage : 1");
+        floors.add("Etage : 2");
+        floors.add("Etage : 3");
+        floors.add("Etage : 4");
+        floors.add("Etage : 5");
+        floors.add("Etage : 6");
+        floors.add("Etage : 7");
+        floors.add("Etage : 8");
+        floors.add("Etage : 9");
+    }
+    public String getSelectedFloor(){
+        return (String) getSelectedItem();
+    }
+
+    @Override
+    public Object getElementAt(int index){
+        return floors.get(index);
+    }
+
+    @Override
+    public int getSize(){
+        return floors.size();
+    }
+
+    @Override
+    public int getIndexOf(Object element){
+        return floors.indexOf(element);
+    }
+}
--- src/IHM.java
+++ src/IHM.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class IHM extends JFrame {
+public class IHM extends JFrame implements ItemListener {
 
     JButton upBtn;
     JButton downBtn;
@@ -14,13 +18,12 @@ public class IHM extends JFrame {
     JButton btnFloor3;
     JButton btnFloor4;
     JButton btnFloor5;
+    JButton btnFloor6;
+    JButton btnFloor7;
+    JButton btnFloor8;
+    JButton btnFloor9;
 
-    JButton Floor0 ;
-    JButton Floor1;
-    JButton Floor2 ;
-    JButton Floor3 ;
-    JButton Floor4 ;
-    JButton Floor5 ;
+    JComboBox floors;
 
 
     public IHM(){
@@ -35,11 +38,13 @@ public class IHM extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-        setVisible(true);
+        commandBtnLeftPanel(leftPanel());
+        floorsBoxLeftPanel(leftPanel());
         leftPanel();
-        floorBtnPanel(rightPanel());
-        commandBtnPanel(rightPanel());
+        floorBtnRightPanel(rightPanel());
+        commandBtnRightPanel(rightPanel());
         rightPanel();
+
 
 
     }
@@ -60,10 +65,10 @@ public class IHM extends JFrame {
         return panel;
     }
 
-    void floorBtnPanel(JPanel panel){
+    void floorBtnRightPanel(JPanel panel){
 
         panel.setBounds(750,50,200,200);
-        GridLayout gridLayout = new GridLayout(3, 2);
+        GridLayout gridLayout = new GridLayout(5, 2);
         gridLayout.setHgap(3);
         gridLayout.setVgap(3);
         panel.setLayout(gridLayout);
@@ -74,9 +79,15 @@ public class IHM extends JFrame {
         btnFloor3 = new JButton("3");
         btnFloor4 = new JButton("4");
         btnFloor5 = new JButton("5");
+        btnFloor6 = new JButton("6");
+        btnFloor7 = new JButton("7");
+        btnFloor8 = new JButton("8");
+        btnFloor9 = new JButton("9");
 
-
-
+        panel.add(btnFloor9);
+        panel.add(btnFloor8);
+        panel.add(btnFloor7);
+        panel.add(btnFloor6);
         panel.add(btnFloor5);
         panel.add(btnFloor4);
         panel.add(btnFloor3);
@@ -87,7 +98,7 @@ public class IHM extends JFrame {
         add(panel);
     }
 
-    void commandBtnPanel(JPanel panel){
+    void commandBtnRightPanel(JPanel panel){
 
         panel.setBounds(730,500,250,100);
         GridLayout gridLayout = new GridLayout(2, 2);
@@ -95,22 +106,74 @@ public class IHM extends JFrame {
         gridLayout.setVgap(3);
         panel.setLayout(gridLayout);
 
-        upBtn = new JButton("UP");
-        downBtn = new JButton("DOWN");
-        stopBtn = new JButton("STOP");
-        stopEmergencyBtn = new JButton("STOP HS");
+        stopEmergencyBtn = new JButton("STOP EMERGENCY");
 
-        panel.add(upBtn);
-        panel.add(downBtn);
-        panel.add(stopBtn);
         panel.add(stopEmergencyBtn);
 
         add(panel);
     }
 
+    void floorsBoxLeftPanel(JPanel panel){
+        floors =new JComboBox(new FloorModel());
+        floors.setPreferredSize(new Dimension(500,50));
+        floors.addItemListener(this);
+        upBtn.setEnabled(false);
+        downBtn.setEnabled(false);
+        panel.add(floors);
+        add(panel);
+    }
 
+    void commandBtnLeftPanel(JPanel panel){
+
+        panel.setBounds(200,80,250,100);
+        upBtn = new JButton("UP");
+        panel.add(upBtn);
+        downBtn = new JButton( "DOWN");
+        panel.add(downBtn);
+        add(panel);
+    }
+
+
+
+    public FloorModel getModelFloor(){ return (FloorModel) floors.getModel();}
 
     public static void main(String[] args){
-        new IHM().build();
+        new IHM().setVisible(true);
     }
+    
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if(e.getSource() == floors){
+            System.out.println(" click ! ! ! ");
+            String floor = getModelFloor().getSelectedFloor();
+            System.out.println(floor);
+            switch (floor){
+                case "Etage : 0":
+                    this.downBtn.setEnabled(false);
+                    this.upBtn.setEnabled(true);
+                    break;
+                case "Etage : 1":
+                case "Etage : 2":
+                case "Etage : 3":
+                case "Etage : 4":
+                case "Etage : 5":
+                case "Etage : 6":
+                case "Etage : 7":
+                case "Etage : 8":
+                    this.upBtn.setEnabled(true);
+                    this.downBtn.setEnabled(true);
+                    break;
+                case "Etage : 9":
+                    this.upBtn.setEnabled(false);
+                    this.downBtn.setEnabled(true);
+                    break;
+
+            }
+        }
+
+
+    }
+
+
 }
